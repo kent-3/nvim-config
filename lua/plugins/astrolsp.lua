4e2eac6ab00a1e50a5a02b46f7579f7cb0c41e14
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-07-23 16:34:42 -0400

    customize astro plugins

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -44,7 +42,24 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      cssls = { settings = { css = { validate = false, lint = { unknownAtRules = "ignore" } } } },
+      tailwindcss = {
+        filetypes = { "html", "css", "javascript", "typescript", "svelte", "rust" },
+        init_options = { userLanguages = { rust = "html" } },
+        settings = { includeLanguages = { rust = "html" } },
+      },
+      rust_analyzer = {
+        settings = {
+          checkOnSave = true,
+          procMacro = {
+            ignored = {
+              leptos_macro = {
+                "server",
+              },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
